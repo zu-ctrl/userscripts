543978cd33f836bbffc92948414bdda3a0ce038d
Petro Kuzmychev <johnnyfearmekramer@gmail.com>
2020-09-29 19:27:21 +0400

    up version

--- nexusmutual2etherscan.user.js
+++ nexusmutual2etherscan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nexusmutual2etherscan
 // @namespace    https://app.nexusmutual.io/
-// @version      0.3
+// @version      0.4
 // @description  Request catcher and form filler
 // @author       johnnykramer
 // @match        *://app.nexusmutual.io/cover/*
